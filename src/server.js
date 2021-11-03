3a82554243d420af4b1378e72756282264bcc5ab
Leonardo-Meirelles <leonardomeirelles@terra.com.br>
2021-11-03 20:31:27 -0300

    bugs correction

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 3333;
 require('./database/index');
 
 const routes = require('./routers/indexRoutes');
@@ -13,4 +12,5 @@ app.use(express.json());
 
 routes(app);
 
-app.listen(port, () => console.log(`server working on ${port}`));
+const port = process.env.PORT || 5000;
+app.listen(port);
